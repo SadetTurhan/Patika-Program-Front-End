61d54b058b72f937a477364290b695f8c2c9eb51
SadetTurhan <msadetturhan1@gmail.com>
2024-04-14 11:11:55 +0300

    alert eklendi

--- week6/to-do-list/app.js
+++ week6/to-do-list/app.js
@@ -1,14 +1,28 @@
 let taskFormDOM = document.querySelector("#taskForm");
 taskFormDOM.addEventListener('submit',formHandler)
 
+const alertDOM = document.querySelector("#alert")
+const ALERT = `
+<div class="alert alert-warning alert-dismissible fade show ml-3" role="alert">
+  <strong>houston we have a problem!</strong> Please fill in the input.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+` 
+const SUCCESSALERT = `
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    Task has been added.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+`
 function formHandler(event){
     event.preventDefault()
     const TASK_NAME = document.querySelector("#taskname")
     if(TASK_NAME.value){
         addTask(TASK_NAME.value)
         TASK_NAME.value = ""
+        alertDOM.innerHTML = SUCCESSALERT;
     }else{
-        console.log("hatalı giriş")
+        alertDOM.innerHTML = ALERT;
     }
 }
 
